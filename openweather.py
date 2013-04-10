c65d72cd91793f13b4889bfc12bc37ec83978491
David Maiorino <maiorinodavid@gmail.com>
2013-04-10 23:01:01 +0900

    Added options for metric/imperial metrics

--- openweather.py
+++ openweather.py
@@ -6,29 +6,31 @@ import json
 import os.path
 import ConfigParser
 import sys
+import argparse
 
 class getWeather():
 
     def get_city_id(self, city):
     
-        city_list = ConfigParser.ConfigParser()
-        city_list.read('./city-id.txt')
+        #city_list = ConfigParser.ConfigParser()
+        #city_list.read('./city-id.txt')
 
-        try:
-            city_id = city_list.get('CityList',city)
-        except:    
-            url = "http://openweathermap.org/data/2.0/find/name?q="+city+"&units=metric"
-            try:
-                f = urllib2.urlopen(url)
-            except:
-                print 'Error, unable to access http://openweathermap.org/data/2.0/find/name?q='+city 
+        #try:
+        #    city_id = city_list.get('CityList',city)
+        #except:    
+	    url = "http://openweathermap.org/data/2.0/find/name?q="+city+"&units=metric"
+
+	    try:
+		f = urllib2.urlopen(url)
+	    except:
+		print 'Error, unable to access http://openweathermap.org/data/2.0/find/name?q='+city 
 
 	    json_data = f.read()
 	    jdata_decoded = json.loads(json_data)
 	    count = jdata_decoded['count']
 
 	    if ( count > 1 ):
-	 	print 'More then one site returned, please select from the following:'	
+		print 'More then one site returned, please select from the following:'	
 		counter = 0
 		while (counter < count):
 			city_id = jdata_decoded['list'][counter]['id']
@@ -46,9 +48,12 @@ class getWeather():
     def get_json_data(self,city):
         
         city_id = self.get_city_id(city)
-	print "City_id is: " + str(city_id)
             
-        url = "http://openweathermap.org/data/2.0/weather/city/"+str(city_id)+"?type=json"
+	if args.farenheight:
+		url = "http://openweathermap.org/data/2.0/find/name?q="+city+"&units=imperial"
+	else:
+		url = "http://openweathermap.org/data/2.0/find/name?q="+city+"&units=metric"
+        	#url = "http://openweathermap.org/data/2.0/weather/city/"+str(city_id)+"?type=json"
         try:
             f = urllib2.urlopen(url)
         except:
@@ -62,17 +67,18 @@ class getWeather():
         json_data = self.get_json_data(city)
         jdata_decoded = json.loads(json_data)
         w_data = {}
-        
-        w_data['id'] = str(jdata_decoded['id'])
-        w_data['name'] = str(jdata_decoded['name'])
-        w_data['date'] = str(jdata_decoded['date'])
-        w_data['temp'] = str(jdata_decoded['main']['temp'] - 273.15)
-        w_data['pressure'] = str(jdata_decoded['main']['pressure']*0.75)+" mmHg"
-        w_data['humidity'] = str(jdata_decoded['main']['humidity'])+"%"
-        w_data['wind'] = str(jdata_decoded['wind']['speed'])+" m/s"
-        w_data['clouds'] = str(jdata_decoded['clouds']['all'])+"%"
-        w_data['img'] = (jdata_decoded['weather'][0]['icon'])
-        w_data['weather'] = (jdata_decoded['weather'][0]['main'])
+
+	w_data['id'] = str(jdata_decoded['list'][0]['id'])
+        w_data['name'] = str(jdata_decoded['list'][0]['name'])
+        w_data['date'] = str(jdata_decoded['list'][0]['date'])
+        w_data['temp'] = str(jdata_decoded['list'][0]['main']['temp'])
+	#print "What"+w_data['temp'] 
+        w_data['pressure'] = str(jdata_decoded['list'][0]['main']['pressure'])
+        w_data['humidity'] = str(jdata_decoded['list'][0]['main']['humidity'])+"%"
+        w_data['wind'] = str(jdata_decoded['list'][0]['wind']['speed'])+" m/s"
+        w_data['clouds'] = str(jdata_decoded['list'][0]['clouds']['all'])+" m/s"
+        w_data['img'] = (jdata_decoded['list'][0]['weather'][0]['icon'])
+        w_data['weather'] = str(jdata_decoded['list'][0]['weather'][0]['main'])
         
         w_iconname = w_data['img'] + ".png"
 
@@ -92,7 +98,10 @@ class getWeather():
         
         weather_data = self.get_weather(city)
         print 'Weather in '+city+''
-        print 'Temperature :',weather_data['temp']+"C"
+	if args.farenheight:
+		print 'Temperature :',weather_data['temp']+ " F"
+	else:
+		print 'Temperature :',weather_data['temp']+ " C"
         print 'Pressure    :',weather_data['pressure']
         print 'Humidity    :',weather_data['humidity']
         print 'Wind        :',weather_data['wind']
@@ -103,4 +112,13 @@ class getWeather():
 if __name__ == "__main__":
 
     app = getWeather()
-    app.main(str(sys.argv[1]))
+
+    #Start option settings#
+    parser = argparse.ArgumentParser()
+    parser.add_argument("city", help="Search this city for weather information")
+    parser.add_argument("-f","--farenheight", action="store_true", help="Set metics to Farenheight")
+    parser.add_argument("-c","--celcius", action="store_true", help="Set metics to Celcius")
+    args = parser.parse_args()
+    #End option settings#
+
+    app.main(str(args.city))
